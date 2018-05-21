2012df2b222a06b902e3aa6f6fb3f534e4d4882d
u1and0 <e01.ando60@gmail.com>
2018-05-21 16:58:43 +0900

    [add]class DataReader

--- csv_reader.py
+++ csv_reader.py
@@ -8,27 +8,50 @@
 
 import pandas as pd
 import os
+
+
+class DataReader:
+    def __init__(self, machine, *filelist):
+        self.machine = machine
+        self.data = {'N5071': reader_N5071, 'A9010': reader_A9010}
+
+        def k(x):
+            return self.data[machine](*x)
+
+        self = k(filelist)
+        self.machine = list(self.data.keys())
+
+
 def reader_N5071(*filelist):
     """ネットワークアナライザN5071からデータインポート"""
     skiprows = 3
-    tempdata = pd.read_csv(filelist[0], skiprows=skiprows,
-                           engine='python', names=['Frequency', 'temp'],
-                           usecols=[0,1], index_col=0)
+    tempdata = pd.read_csv(
+        filelist[0],
+        skiprows=skiprows,
+        engine='python',
+        names=['Frequency', 'temp'],
+        usecols=[0, 1],
+        index_col=0)
     df = pd.DataFrame(tempdata)
     for file in filelist:
-        dff = pd.read_csv(file, skiprows=skiprows-1, engine='python',
-                          usecols=[0,1], index_col=0)
+        dff = pd.read_csv(
+            file,
+            skiprows=skiprows - 1,
+            engine='python',
+            usecols=[0, 1],
+            index_col=0)
         basename = os.path.splitext(os.path.basename(file))[0]
         df[basename] = dff
     del df['temp']
     return df
 
+
 def reader_A9010(*filelist):
     skiprows = 44
     # 1ファイルだけ読み込んでindex設定用に使う
     tempfile = filelist[0]
-    tempdata = pd.read_csv(tempfile, skiprows=skiprows, engine='python',
-                           names=['temp'])
+    tempdata = pd.read_csv(
+        tempfile, skiprows=skiprows, engine='python', names=['temp'])
     df = pd.DataFrame(tempdata)
     for file in filelist:
         dff = pd.read_csv(file, skiprows=skiprows - 1, engine='python')
@@ -36,19 +59,3 @@ def reader_A9010(*filelist):
         df[basename] = dff
     del df['temp']
     return df
-
-def nearest_x(df, value):
-    """valueに最も近い値下がったところのindexを返す"""
-    down = df.max() - value
-    absolute_sub = pd.DataFrame(abs(df - down)).sort_values(by=df.columns[0])
-    return absolute_sub
-
-
-# 残骸
-# class DataReader:
-#     machine=(A_9010, N_5071)
-#     __init__(file, machine)
-#         if not machine
-#         machine=self.machine
-
-
